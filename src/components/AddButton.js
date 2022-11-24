ce977f03e66e7f89f9cc343037b10b147124de58
Lisa <lshort24@gmailcom>
2022-11-23 17:25:20 -0700

    Update style for add button

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,48 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/styles';
+// Material UI components
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
 // Components
-import { Add } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 
-const styles = {
-    addButton: {
-        display: 'flex',
-    },
-    addIcon: {
-        border: '1px solid #7c9c60',
-        borderRadius: '5px',
-        backgroundColor: 'white',
-        marginRight: '8px',
-        height: '24px',
-        width: '24px',
-        color: '#7c9c60',
-    },
-    addLink: {
-        height: '24px',
-        lineHeight: '24px',
-        color: '#7c9c60',
-    },
-}
-
-function AddButton({classes}) {
+function AddButton() {
     return (
         <Link to="new">
-            <div className={classes.addButton}>
-                <div>
-                    <Add className={classes.addIcon}/>
-                </div>
-                <div className={classes.addLink}>
-                    Add Recipe
-                </div>
-            </div>
+            <Button variant="contained" startIcon={<AddIcon />}>
+                Add Recipe
+            </Button>
         </Link>
     )
 }
 
-AddButton.propTypes = {
-    classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(AddButton);
+export default AddButton;
--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ import createSagaMiddleware from 'redux-saga';
 
 // Style
 import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 // Components
 import App from './App';
